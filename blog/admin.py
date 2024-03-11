b39b18857feeeeacc726b0148f67256708ac4d86
snehal <you@example.com>
2024-03-11 15:44:21 +0530

    setup custom admin sites

--- blog/admin.py
+++ blog/admin.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from . import models
 
-# Register your models here.
+
 class BlogAdminArea(admin.AdminSite):
     site_header="Blog Admin Area"
 
-blog_site=BlogAdminArea(name="BlogAdmin")
+
+blog_site=BlogAdminArea(name="Blog Admin")
 
 admin.site.register(models.Post)
 blog_site.register(models.Post)
--- bookstore/admin.py
+++ bookstore/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+class BookstoreAdminArea(admin.AdminSite):
+    site_header="Book Store Admin Area"
+
+bookstore_site=BookstoreAdminArea(name="BookstoreAdmin")
